4714d98065f83e395456ed1abdc36c8fb11c92b0
Ángel D <esdrexd.106@gmail.com>
2020-05-14 22:10:04 -0600

    includes

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 #include <cstdlib>
 #include <conio.h>
 #include <windows.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+#include "Torretas.h"
+#include "Enemigo.h"
+#include "Ciudad.h"
+#include "Bala.h"
 using namespace std;
 void SetColor(int ForgC) 
 { 
