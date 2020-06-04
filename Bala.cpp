c54d0746a1a3de1737d6d58599474d25419ea3b3
Nyle-ve <evgo_19@alu.uabcs.mx>
2020-06-03 21:36:13 -0600

    Bala

--- Bala.cpp
+++ Bala.cpp
@@ -15,20 +15,43 @@ void gotoxy5(int x, int y){
 	dwPos.Y = y;
 	SetConsoleCursorPosition(hCon, dwPos);
 }
-Bala::Bala(int _x, int _y){
+Bala::Bala(){}
+void Bala::crear(int _x, int _y, int _id){
 	this->x = _x;this->ini_x = _x;
 	this->y = _y;this->ini_y = _y;
-	this->dy = -1;
+	this->id = _id;
+	if (id == 0 || id == 1 || id == 2){ // disparo hacia arriba
+		this->dy = -1;
+	}else if(id == 3){ // disparo hacia abajo
+		this->dy = 1;
+	}
+	if(id == 1){ // movimiento diagonal derecho
+		this->dx = 3;
+	}else if (id == 2){ //movimiento diagonal izquierdo
+		this->dx = -3;
+	}
+	
 }
 void Bala::pintar(){
 	gotoxy5(x,y);printf("*");
 }
 void Bala::movimiento(){
-	
-	if(this->y != 0){
-		this->y += this->dy;
-	}else{
-		this->y = this->ini_y;
+	if(id == 0){ //mover hacia arriba
+		if(this->y != 0){ //regresa a posici?n inicial
+			this->y += this->dy;
+		}else{
+			this->y = this->ini_y;
+		}
+	}else if(id == 1 || id == 2){ //mover en diagonales 
+		if(this->y != 0){ //regresa a posici?n inicial
+			this->y += this->dy;
+			this->x += this->dx; 
+		}else{
+			this->y = this->ini_y;
+			this->x = this->ini_x;
+		}
+	}else if(id == 3){ //mover hacia abajo
+			this->y += this->dy;
 	}
 }
 void Bala::borrar(){
@@ -40,6 +63,10 @@ int Bala::xbala(){
 int Bala::ybala(){
 	return this->y;
 }
+int Bala::setX(int _newX){
+	this->x = _newX;
+}
 int Bala::setY(int _newY){
 	this->y = _newY;
 }
+
--- Bala.h
+++ Bala.h
@@ -2,16 +2,18 @@
 #ifndef UNTITLED_BALA_H
 #define UNTITLED_BALA_H
 
-
+#include "Enemigo.h"
 class Bala {
 	private:
 		int x;
 		int y;
+		int id;
 		int ini_x, ini_y;
 		int dx;
 		int dy;
 	public:
-		Bala(int, int);
+		Bala();
+		void crear(int, int, int);
 		void pintar();
 		void movimiento();
 		void borrar();
@@ -23,3 +25,4 @@ class Bala {
 
 
 #endif //UNTITLED_CIUDAD_H
+
